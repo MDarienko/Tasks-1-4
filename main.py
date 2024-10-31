5b25be95e04a60796156fae5889d8532fa008fb1
MDarienko <michd0101@gmail.com>
2024-10-31 17:40:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-name = "Mikhail"
-print (name)
-age = 23
-print (age)
-age = (23+1)
-print (age)
-bool("is student")
-print (bool("is student"))
+homework_tasks_done = 12
+hours_spent = 1.5
+courses_name = "python"
+time_on_1_task = (hours_spent / homework_tasks_done)
+print (courses_name, homework_tasks_done, hours_spent, time_on_1_task)
